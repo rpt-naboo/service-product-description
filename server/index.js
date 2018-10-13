6445e78397993332e24b65cf3cb8325a0a6da74d
jonjharrison <jj2harrison@gmail.com>
2018-10-13 16:47:17 -0700

    Return all descriptions

--- server/index.js
+++ server/index.js
@@ -2,6 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const pg = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    user: 'jonny',
+    password: 'jonny',
+    database: 'product_descriptions'
+  }
+});
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/descriptions', (req, res) => {
+  pg.select().table('descriptions')
+  .then(function(rows) {
+    res.send(rows);
+  });
+});
 
 app.listen(port, () => console.log(`Server is now listening on port ${port}`));
