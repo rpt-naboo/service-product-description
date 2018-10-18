392e805a1364273ba118cae7e49e62945b65f09c
jonjharrison <jj2harrison@gmail.com>
2018-10-17 19:26:29 -0700

    Add stubs added to object, product details function

--- data/seed.js
+++ data/seed.js
@@ -2,10 +2,10 @@
 function generateDescription(productId) {
   return {
     productSize: productSize(),
-    productColor: '',
-    productDetails: '',
-    packagingType: '',
-    productImageId: ''
+    productColor: productColor(),
+    productDetails: productDetails(),
+    packagingType: packagingType(),
+    productImageId: productImageId()
   };
 }
 
@@ -54,6 +54,19 @@ function productColor() {
 }
 
 function productDetails() {
+  var details = {
+    0: 'Lorem ipsum dolor amet banh mi paleo shaman chillwave, incididunt keytar lo-fi ipsum crucifix keffiyeh. Deep v anim cred pitchfork tote bag meh drinking vinegar portland kogi +1 do artisan hot chicken austin irure. Id fashion axe microdosing, stumptown wolf elit commodo in cardigan. Wayfarers drinking vinegar umami air plant tbh aliquip pabst deserunt. Vice ad portland gastropub waistcoat excepteur put a bird on it cred skateboard prism plaid microdosing pitchfork cold-pressed. Selvage wolf vice jianbing +1 blog pour-over fixie green juice.',
+    1: 'Actually affogato keytar artisan aute kickstarter master cleanse tumblr. PBR&B +1 truffaut echo park raclette gochujang blue bottle exercitation chillwave edison bulb cronut occaecat adipisicing. Cred pork belly organic sint. Live-edge hammock put a bird on it laboris ex wolf ullamco. Tofu taxidermy vexillologist shaman keytar ethical ex. Affogato leggings ut beard non lyft celiac, knausgaard banjo occaecat umami microdosing lo-fi. Reprehenderit kinfolk tempor, viral vaporware synth microdosing tattooed mumblecore.',
+    2: 'Tumblr velit sint, do normcore labore pickled aute actually artisan poutine salvia XOXO tilde. Neutra lo-fi magna dolore food truck succulents af est. Consectetur elit kitsch mustache, ex microdosing cray sriracha deserunt chia. Blog exercitation hot chicken, authentic umami esse chambray distillery bespoke. Hot chicken venmo humblebrag pinterest echo park banh mi occaecat hoodie locavore man braid pop-up. Listicle excepteur aliquip hexagon, whatever selfies shoreditch art party kogi. Affogato quinoa consequat selfies, flannel chambray pug jean shorts magna.',
+    3: 'Minim qui cardigan, lo-fi blog in irony dolor cupidatat 90\'s. Distillery seitan gochujang scenester slow-carb est mollit freegan hoodie roof party aliquip lorem coloring book fashion axe. Velit unicorn asymmetrical officia. Pop-up 3 wolf moon poke before they sold out ad. Mlkshk messenger bag craft beer, lorem fam voluptate master cleanse drinking vinegar photo booth cloud bread gochujang nisi poutine. Post-ironic cronut meditation, non leggings truffaut four dollar toast hoodie tilde palo santo cornhole occaecat keffiyeh shabby chic.',
+    4: 'Cardigan food truck sint edison bulb mumblecore woke. Pour-over post-ironic sed, yr quinoa ennui everyday carry jean shorts aliqua etsy next level bespoke brooklyn vaporware. Velit meggings flexitarian, ut proident fixie sunt cold-pressed microdosing shaman echo park pariatur taiyaki. Biodiesel deserunt viral XOXO. Drinking vinegar freegan lyft lumbersexual before they sold out.',
+  }
+
+  var randomIndex = Math.floor(Math.random() * 5);
+
+  if (details[randomIndex]) {
+    return details[randomIndex];
+  }
 
 }
 
