68676a0cb4b79d1916756e971074c6113b53b4c2
jonjharrison <jj2harrison@gmail.com>
2018-10-20 12:08:24 -0700

    Working off of style.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello World! </h1>
+        <h1> Hello Jonny! </h1>
       </div>
     );
   }
