13c8e43edd3f515ddcbe95d834e80363d4877994
jonjharrison <jj2harrison@gmail.com>
2018-10-20 14:33:51 -0700

    Header color to match amazon

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.js';
-ReactDOM.render(<App />, document.getElementById('root'));
+import ProductDescription from './components/ProductDescription';
+ReactDOM.render(<ProductDescription />, document.getElementById('root'));
