efa052970730598c0c01ea26c4a75fe22d9aacc3
jonjharrison <jj2harrison@gmail.com>
2018-10-20 14:33:34 -0700

    Update component name

--- client/src/components/ProductDescription.js
+++ client/src/components/ProductDescription.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import '../style.css';
 
-class App extends Component {
+class ProductDescription extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello Larry! </h1>
+        <h1> Product Description </h1>
       </div>
     );
   }
 }
 
-export default App;
+export default ProductDescription;
