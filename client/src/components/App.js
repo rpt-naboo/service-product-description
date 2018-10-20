e00220e6c058658507b3f96a54596074f7be79fd
jonjharrison <jj2harrison@gmail.com>
2018-10-20 12:10:15 -0700

    Cleanup into components folder

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './style.css';
+import '../style.css';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello Jonny! </h1>
+        <h1> Hello Larry! </h1>
       </div>
     );
   }
