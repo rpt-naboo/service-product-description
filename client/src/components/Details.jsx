e02f61e577763e07f7299aeba2308ce9b260d726
jonjharrison <jj2harrison@gmail.com>
2018-10-23 21:31:11 -0700

    Change details to stateful component

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Details = () => {
+class Details extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-  return (
-    <div>
-      <h2>Description:</h2>
-      <h2>Product Size:</h2>
-      <h2>Product Color:</h2>
-    </div>
-  )
+  componentDidMount() {
+    this.getProductDetails();
+  }
+
+  getProductDetails() {
+    let that = this;
+
+    fetch('http://localhost:1337/1/descriptions')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(details) {
+      console.log('This is where the response goes', details[0]);
+      that.setState({
+        details: details[0],
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Description:</h2>
+        <h2>Product Size:</h2>
+        <h2>Product Color:</h2>
+      </div>
+    );  
+  }
 }
 
 export default Details;
--- client/src/components/ProductDescription.jsx
+++ client/src/components/ProductDescription.jsx
@@ -4,41 +4,12 @@ import Keywords from './Keywords.jsx';
 import '../style.css';
 
 class ProductDescription extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      details: {},
-    }
-  }
-
-  componentDidMount() {
-    this.getProductDetails();
-  }
-
-  getProductDetails() {
-    let that = this;
-
-    fetch('http://localhost:1337/1/descriptions')
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(details) {
-      console.log('This is where the response goes', details[0]);
-      that.setState({
-        details: details[0],
-      });
-    });
-  }
-
   render() {
     return (
       <div className='App'>
         <h1> Product Descriptions </h1>
-        <div></div>
-
         <Keywords />
-        <Details details={this.details}/>
+        <Details />
       </div>
     );
   }
