432b3aad82151450457a93ce6963811b1b3395c4
jonjharrison <jj2harrison@gmail.com>
2018-10-23 21:47:20 -0700

    Fully rendering a single record

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 class Details extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      details: '',
+      productColor: '',
+      packagingType: '',
+      productSize: '',
+      productImage: '',
+    }
   }
 
   componentDidMount() {
@@ -16,10 +24,16 @@ class Details extends Component {
     .then(function(response) {
       return response.json();
     })
-    .then(function(details) {
-      console.log('This is where the response goes', details[0]);
+    .then(function(detailArray) {
+      let details = detailArray[0];
+      console.log('This is where the response goes', details);
+
       that.setState({
-        details: details[0],
+        details: details.details,
+        productColor: details.product_color,
+        packagingType: details.packaging_type_id,
+        productSize: details.product_size || 'N/A',
+        productImage: details.product_image_id,
       });
     });
   }
@@ -28,8 +42,13 @@ class Details extends Component {
     return (
       <div>
         <h2>Description:</h2>
+          <p>{this.state.details}</p>
+        <h2>Packaging Type:</h2>
+          <p>{this.state.packagingType}</p>
         <h2>Product Size:</h2>
+          <p>{this.state.productSize}</p>
         <h2>Product Color:</h2>
+          <p>{this.state.productColor}</p>
       </div>
     );  
   }
