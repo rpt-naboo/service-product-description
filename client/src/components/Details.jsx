aa1489f525c01ec557b400419d8b0a8a522e0b6f
jonjharrison <jj2harrison@gmail.com>
2018-10-27 16:08:53 -0700

    Conditional rendering for everything, actual package label

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -30,7 +30,7 @@ class Details extends Component {
       that.setState({
         details: details.details,
         productColor: details.product_color,
-        packagingType: details.packaging_type_id,
+        packagingType: details.packaging_type_label,
         productSize: details.product_size || 'N/A',
         productImage: details.product_image_url,
       });
@@ -40,9 +40,18 @@ class Details extends Component {
   render() {
     return (
       <div>
-        <h2>Packaging Type: {this.state.packagingType}</h2>
-        <h2>Product Size: {this.state.productSize}</h2>
-        <h2>Product Color: {this.state.productColor}</h2>
+        {
+          this.state.packagingType &&
+          <h2>Packaging Type: {this.state.packagingType}</h2>
+        }
+        {
+          this.state.productSize &&
+          <h2>Product Size: {this.state.productSize}</h2>
+        }
+        {
+          this.state.productColor &&
+          <h2>Product Color: {this.state.productColor}</h2>
+        }
         <p>{this.state.details}</p>
         {
           this.state.productImage &&
