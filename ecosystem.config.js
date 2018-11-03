f4eb2388ad8bbc73aadd74129c1a01d1ce19e3ee
jonjharrison <jj2harrison@gmail.com>
2018-11-03 16:37:14 -0700

    Additional config to be able to deploy with PM2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,18 @@ module.exports = {
     env: {
       NODE_ENV: 'development',
     }
-  }]
+  }],
+  deploy: {
+    production: {
+      key: '~/.ssh/hack-reactor/hack-reactor-key.pem',
+      user: 'ubuntu',
+      host: [52.34.129.254],
+      ref: 'origin/master',
+      repo: 'git@github.com:rpt-naboo/service-product-description.git',
+      path: '/home/ubuntu/service-product-description',
+      post-deploy: 'npm install && npm run react-dev',
+    }
+
+  }
 }
 
