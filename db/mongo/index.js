d42d6a45e155d9c8c0ffd1ed2e4eaaac856648db
jonjharrison <jj2harrison@gmail.com>
2018-11-28 20:46:44 -0800

    Event hooks

--- db/mongo/index.js
+++ db/mongo/index.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
-
 mongoose.connect(process.env.MONGO_DB);
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Successfully hooked into mongoose');
+
+
+  
+});
