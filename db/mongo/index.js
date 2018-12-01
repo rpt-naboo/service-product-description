3d16b4667d2ea486b31230d57b7d843842153a71
jonjharrison <jj2harrison@gmail.com>
2018-12-01 14:10:30 -0800

    Updated exports, moved Models out of connection listener

--- db/mongo/index.js
+++ db/mongo/index.js
@@ -8,24 +8,25 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('Successfully hooked into mongoose');
-
-  let descriptionSchema = mongoose.Schema({
-    size: String,
-    color: String,
-    details: String,
-    packagingTypeId: Number,
-    productImageUrl: String,
-  });
-
-  let Description = mongoose.model('Description', descriptionSchema);
-
-  let packagingTypeSchema = mongoose.Schema({
-    label: String,
-  });
-
-  let PackagingType = mongoose.model('PackagingType', packagingTypeSchema);
-  
+  console.log('Successfully hooked into mongoose');  
 });
 
+let descriptionSchema = mongoose.Schema({
+  size: String,
+  color: String,
+  details: String,
+  packagingTypeId: Number,
+  productImageUrl: String,
+});
+
+let Description = mongoose.model('Description', descriptionSchema);
+
+let packagingTypeSchema = mongoose.Schema({
+  label: String,
+});
+
+let PackagingType = mongoose.model('PackagingType', packagingTypeSchema);
+
 module.exports.db = db;
+module.exports.Description = Description;
+module.exports.PackagingType = PackagingType;
