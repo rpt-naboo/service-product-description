d2947c11442dbc4d97bbd9d852d74248a55243a6
jonjharrison <jj2harrison@gmail.com>
2018-12-01 14:03:13 -0800

    Added models

--- db/mongo/index.js
+++ db/mongo/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 mongoose.connect(process.env.MONGO_DB);
 const db = mongoose.connection;
 
@@ -8,6 +10,22 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Successfully hooked into mongoose');
 
+  let descriptionSchema = mongoose.Schema({
+    size: String,
+    color: String,
+    details: String,
+    packagingTypeId: Number,
+    productImageUrl: String,
+  });
 
+  let Description = mongoose.model('Description', descriptionSchema);
+
+  let packagingTypeSchema = mongoose.Schema({
+    label: String,
+  });
+
+  let PackagingType = mongoose.model('PackagingType', packagingTypeSchema);
   
 });
+
+module.exports.db = db;
